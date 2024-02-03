02a9e554ea6744a811e3496df39fc87137f667a9
Luiz Victor <luizvictor.dev@outlook.com>
2024-02-03 10:28:46 -0300

    feat: first commit

---
+++ src/main/java/br/com/luizvictor/anotai/AnotaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizvictor.anotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/luizvictor/anotai/AnotaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizvictor.anotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
