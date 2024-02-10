cc04b5e800fe544664dcdd5240b82a1cfec2f189
Luiz Victor <luizvictor.dev@outlook.com>
2024-02-10 16:04:17 -0300

    feat: adding rabbitmq

---
+++ src/main/java/br/com/luizvictor/anotai/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package br.com.luizvictor.anotai.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    private final CachingConnectionFactory connectionFactory;
+
+    public RabbitMQConfig(CachingConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Bean
+    public Queue catalogGenerationQueue() {
+        return new Queue("q.catalog-generation");
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(Jackson2JsonMessageConverter converter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(converter);
+        return template;
+    }
+}
--- src/main/java/br/com/luizvictor/anotai/services/CatalogService.java
+++ src/main/java/br/com/luizvictor/anotai/services/CatalogService.java
@@ -3,35 +3,49 @@ package br.com.luizvictor.anotai.services;
 import br.com.luizvictor.anotai.entities.category.Category;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 @Service
 public class CatalogService {
     private final CategoryService service;
+    private final RabbitTemplate rabbitTemplate;
+    private final Logger logger = LoggerFactory.getLogger(CatalogService.class);
 
-    public CatalogService(CategoryService service) {
+    public CatalogService(CategoryService service, RabbitTemplate rabbitTemplate) {
         this.service = service;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     public String create(Long ownerId) {
         List<Category> categories = service.findByOwnerId(ownerId);
-
         JSONObject json = createCatalogJson(ownerId, categories);
-        saveCatalogFile(ownerId, json);
 
+        rabbitTemplate.convertAndSend("", "q.catalog-generation", json.toString());
+        logger.info("The catalog for the owner {} is being created", ownerId);
         return "Catalog is being generated";
     }
 
-    private void saveCatalogFile(Long ownerId, JSONObject json) {
+    @RabbitListener(queues = "q.catalog-generation")
+    public void saveCatalogFile(JSONObject json) {
+        logger.info("Catalog event received: {}", json);
+        Integer ownerId = (Integer) json.get("owner");
+
         try {
-            Files.write(Paths.get("uploads/" + ownerId + "-catalog.json"), json.toString().getBytes());
+            Path path =  Paths.get("uploads/" + ownerId + "-catalog.json");
+            Files.write(path, json.toString().getBytes());
+            logger.info("Catalog saved on: {}", path);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            logger.error("Error generating catalog: {}", e.getMessage());
         }
     }
 
@@ -41,8 +55,9 @@ public class CatalogService {
 
         JSONArray array = new JSONArray();
         for (Category category : categories) {
-           array.put(category.toJson());
+            array.put(category.toJson());
         }
+
         json.put("catalog", array);
 
         return json;
